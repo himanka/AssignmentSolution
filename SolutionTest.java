44aeeccd874ae86d9464545ddde0541050de81af
himanka <himanka619@gmail.com>
2019-02-24 16:41:14 +0800

    Test cases for question 2

--- SolutionTest.java
+++ SolutionTest.java
@@ -6,15 +6,39 @@ import org.junit.Test;
 public class SolutionTest {
 
 	Bird bird;
+	Duck duck;
+	Chicken chicken;
 
 	@Before
 	public void setUp() throws Exception {
 		bird = new Bird();
+		duck =  new Duck();
+		chicken= new Chicken();
 	}
 
 	@Test
-	public void testSing() {
+	public void testBirdSing() {
 		bird.sing();
 	}
+	
+	@Test
+	public void testDuckSwim() {
+		duck.swim();
+	}
 
+	
+	@Test
+	public void testDuckSing() {
+		duck.sing();
+	}
+	
+	@Test
+	public void testChickenSing() {
+		chicken.sing();
+	}
+	
+	@Test
+	public void testChickenFly() {
+		chicken.fly();
+	}
 }
