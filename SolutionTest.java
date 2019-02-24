cf8dff9bf1e7400b8cc5679e937b349db31217d7
himanka <himanka619@gmail.com>
2019-02-24 17:14:21 +0800

    test case changes for question 3  a and b

--- SolutionTest.java
+++ SolutionTest.java
@@ -8,37 +8,62 @@ public class SolutionTest {
 	Bird bird;
 	Duck duck;
 	Chicken chicken;
+	Rooster rooster;
 
 	@Before
 	public void setUp() throws Exception {
 		bird = new Bird();
 		duck =  new Duck();
 		chicken= new Chicken();
+		rooster = new Rooster();
 	}
 
 	@Test
 	public void testBirdSing() {
+		System.out.println("test case testBirdSing");
 		bird.sing();
+		System.out.println("\n");
 	}
 	
 	@Test
 	public void testDuckSwim() {
+		System.out.println("test case testDuckSwim");
 		duck.swim();
+		System.out.println("\n");
 	}
 
 	
 	@Test
 	public void testDuckSing() {
+		System.out.println("test case testDuckSing");
 		duck.sing();
+		System.out.println("\n");
 	}
 	
 	@Test
 	public void testChickenSing() {
+		System.out.println("test case testChickenSing");
 		chicken.sing();
+		System.out.println("\n");
 	}
 	
 	@Test
 	public void testChickenFly() {
+		System.out.println("test case testChickenFly");
 		chicken.fly();
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void testRoosterSing() {
+		System.out.println("test case testRoosterSing");
+		rooster.sing();
+		System.out.println("\n");
+	}
+	
+	@Test
+	public void testRoosterFly() {
+		rooster.fly();
+		System.out.println("\n");
 	}
 }
