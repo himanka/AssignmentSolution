5c52f63c497d82ecaf395069a4848f014536d748
himanka <himanka619@gmail.com>
2019-02-24 16:40:38 +0800

    Answers to question 2

--- Solution.java
+++ Solution.java
@@ -3,10 +3,9 @@ package com.solution;
 public class Solution {
 
 	public static void main(String[] args) {
-		Bird bird = new Bird();
-		bird.walk();
-		bird.fly();
-		bird.sing();
+		/*
+		 * Bird bird = new Bird(); bird.walk(); bird.fly(); bird.sing();
+		 */
 	}
 
 }
@@ -17,12 +16,38 @@ abstract class Animal {
 	}
 }
 
-class Bird extends Animal {
-	void fly() {
-		System.out.println("I am flying");
+ class Bird extends Animal {
+	 void fly() {
+			System.out.println("I am flying");
+		}
+		
+		void sing(){
+			System.out.println("I am Singing");
+	}
+}
+
+class Chicken extends Bird {
+
+	@Override
+	void fly() {}
+
+	@Override
+	void sing() {
+		System.out.println("Chicken sings Cluck, cluck");
+
+	}
+
+}
+
+class Duck extends Bird {
+
+	@Override
+	void sing() {
+		System.out.println("Duck sings Quack, quack");
 	}
 	
-	void sing(){
-		System.out.println("I am Singing");
+	void swim(){
+		System.out.println("Duck is swiming.");
 	}
+
 }
