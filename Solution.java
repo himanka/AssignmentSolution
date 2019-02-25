7ec2d07adf550faf483841631ed6df4f16dda4a1
himanka <himanka619@gmail.com>
2019-02-25 13:25:44 +0800

    solution for Question B  section 1 and 2

--- Solution.java
+++ Solution.java
@@ -3,9 +3,9 @@ package com.solution;
 public class Solution {
 
 	public static void main(String[] args) {
-		/*
-		 * Bird bird = new Bird(); bird.walk(); bird.fly(); bird.sing();
-		 */
+		
+		  Bird bird = new Bird(); bird.walk(); bird.fly(); bird.sing();
+		 
 	}
 
 }
